ca400f997b0b5538c96c49f8654a192829afaa86
Kyle Lee <kyle_lee@live.com>
2022-02-13 15:38:42 -0800

    colors

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,8 +9,13 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Have fun, hope you enjoy the game :) -{' '}
-        
+        Have fun, hope you enjoy the game :) {' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
       </p>
     </BaseModal>
   )
